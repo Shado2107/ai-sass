74c713dc58620384f66b5776e71674556e2bc479
Shado2107 <pd39almeida@outlook.com>
2023-08-30 16:39:35 +0000

    conversation ui

--- components/heading.tsx
+++ components/heading.tsx
@@ -17,8 +17,7 @@ export const Heading = ({
     iconColor,
     bgColor
 }: HeadingProps) => {
-    return (
-        <>
+    return ( 
         <div className="px-4 lg:px-8 flex items-center gap-x-3 mb-8">
            <div className={cn("p-2 w-fit rounded-md", bgColor)} >
                 <Icon className={cn("w-10 h-10", 
@@ -28,9 +27,10 @@ export const Heading = ({
                 <h2 className="text-2xl font-bold">
                     {title}
                 </h2>
+                <p className="text-sm text-muted-foreground">
+                    {description}
+                </p>
             </div>
         </div>
-        
-        </>
     )
 }
